c6d078ea7b4d0e4945a985a308b72cf2153d0283
Michael Heide <mail@heidemichael.de>
2023-01-22 17:22:23 +0100

    extracted SortView in new file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 import 'package:flutter_test_app/pages/Scanbot.dart';
+import 'package:flutter_test_app/pages/SortView.dart';
 
 void main() => runApp(const MyApp());
 
@@ -118,71 +119,6 @@ class Page extends StatelessWidget {
             ),
           ],
         )),
-      ),
-    );
-  }
-}
-
-class SortView extends StatefulWidget {
-  @override
-  _SortViewState createState() => _SortViewState();
-}
-
-class _SortViewState extends State<SortView> {
-  List<int> _list = [];
-  double _timeTaken = 0.0;
-
-  void bubbleSort(List<int> list) {
-    print(" istarted");
-    for (int i = 0; i < list.length; i++) {
-      for (int j = 0; j < list.length - i - 1; j++) {
-        if (list[j] > list[j + 1]) {
-          int temp = list[j];
-          list[j] = list[j + 1];
-          list[j + 1] = temp;
-        }
-      }
-    }
-  }
-
-  void generateAndSortList() {
-    var rng = Random();
-    var list = List<int>.generate(100000, (_) => rng.nextInt(100000));
-    setState(() {
-      _list = list.toSet().toList();
-    });
-    var startTime = DateTime.now();
-    bubbleSort(_list);
-    var endTime = DateTime.now();
-    setState(() {
-      _timeTaken = endTime.difference(startTime).inMilliseconds.toDouble();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Sort View'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text('Time taken: ${_timeTaken.toStringAsFixed(3)} ms'),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _list.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(_list[index].toString()),
-                );
-              },
-            ),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: generateAndSortList,
-        child: Icon(Icons.sort),
       ),
     );
   }
